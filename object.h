6448b2070f577f307dc11df53b91c7c1151808aa
haneensa <hamohammed.sa@gmail.com>
2017-01-05 12:57:15 +0300

    vbo

--- object.h
+++ object.h
@@ -2,6 +2,7 @@
 #define OBJECT_H
 
 #include <QVector3D>
+#include <QOpenGLBuffer>
 
 enum class Object_t { AXON, DENDRITE, BOUTON, SPINE, MITO, SYNAPSE, ASTROCYTE, GLYCOGEN };
 
@@ -12,12 +13,16 @@ public:
     void add_vertex(QVector3D vertex);
     std::vector<QVector3D> getVertices();
     Object_t getObjectType(std::string name);
-
+    bool update_vbo();
+    bool allocate_vbo();
 private:
     Object_t                m_object_t;     /* object type */
     std::string             m_name;
     int                     m_ID;
     std::vector<QVector3D>  m_vertices;
+
+    // either one per mesh or one for the whole object
+    QOpenGLBuffer               m_vbo;
 };
 
 #endif // OBJECT_H
