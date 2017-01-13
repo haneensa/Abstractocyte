ad78e831bf420d8b1e16876859c1332c7f5d9774
Haneen Mohammed <hamohammed.sa@gmail.com>
2017-01-13 18:31:11 +0300

    pass ID to shaders

--- scripts/process_obj.py
+++ scripts/process_obj.py
@@ -8,7 +8,7 @@ import re
 
 # todo: I need to subtract only if we are removing the object
 # open new obj file 
-f = open("../data/mouse03_remove_offsets.obj", 'w')
+f = open("../data/mouse03_neurites.obj", 'w')
 flag = 0
 count = 0
 vcount = 0
@@ -21,31 +21,31 @@ with open("../data/mouse03.obj", "r") as fin:
             name = name.lower()
             tokens = re.findall(r"([\w_']*)", name)
             tokens = tokens[0].split('_')
-            if ("glycogen" in tokens or "surf" in tokens or "curve" in tokens or 
-                "surface" in tokens or 'length' in tokens or 'sphere' in tokens or 'submesh' in tokens): 
+            if (name[:3] == "ast" or "glycogen" in tokens or "surf" in tokens or "curve" in tokens or
+                "surface" in tokens or "length" in tokens or "sphere" in tokens or "submesh" in tokens):
                 flag = 0
                 count = count + 1
-                print "prev_count: ", prev_count, " vcount: ", vcount
-                prev_count = prev_count + vcount
-                vcount = 0
             else:
                 flag = 1
                 f.write(line)  
 
+            print name, " prev_count: ", prev_count, " vcount: ", vcount
+            prev_count = prev_count + vcount
+            vcount = 0
             # decide the object type
             # add new description ex: "t astrocyte" -> type
             # add new description ex: "d 754" -> ID
         elif (tokens[0] == 'v'):
             if (flag == 1):
                 f.write(line)
-            else:
-                vcount = vcount + 1
+            
+            vcount = vcount + 1
         elif (tokens[0] == 'f'):
             if (flag == 1):
                 face_number = [0, 0, 0]
-                face_number[0] = int(tokens[1]) - prev_count
-                face_number[1] = int(tokens[2]) - prev_count
-                face_number[2] = int(tokens[3]) - prev_count
+                face_number[0] =  int(tokens[1]) - prev_count
+                face_number[1] =  int(tokens[2]) - prev_count
+                face_number[2] =  int(tokens[3]) - prev_count
                 newLine = tokens[0] + " " + str(face_number[0]) + " " + str(face_number[1])  + " " + str(face_number[2]) + "\n"
                 f.write(newLine)
 
