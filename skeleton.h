2f9c7bdcb52f01e62e3fcf3fa027ee84cad31adf
Haneen Mohammed <hamohammed.sa@gmail.com>
2017-02-14 13:30:40 +0300

    checkpoint before changing skeleton import

--- skeleton.h
+++ skeleton.h
@@ -24,7 +24,7 @@ public:
     // points has their own IDs [0, Max] for each object and they are distincts per segment
     // segments has IDs but they are not useful to track anything, so I can use any order
 
-    std::vector< QVector3D >    m_skeleton_vertices;
+    std::vector< QVector3D >    m_nodes; // vector or map?
 
 };
 
--- skeletonbranch.h
+++ skeletonbranch.h
@@ -1,14 +1,19 @@
 #ifndef SKELETONBRANCH_H
 #define SKELETONBRANCH_H
 
+#include "mainopengl.h"
+
+// each branch has two knots (nodes) and set of points between them
+// more than a branch can share the same knots
+// but the set of point are unique per branch
 class SkeletonBranch
 {
 public:
     SkeletonBranch();
 
 protected:
-//    std::vector<QVector3D>  m_vertices;
-//    std::vector<QVector2D>  m_lines;
+    std::vector<int> m_nodes; // -> how about each skeleton has set of nodes, and in the branch they refrence them
+    std::vector<QVector2D> m_points; // the points of this branch
 };
 
 #endif // SKELETONBRANCH_H
