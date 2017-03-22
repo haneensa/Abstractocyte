f458c0e8466a09a022e79890580ea8faa0bd4216
Haneen Mohammed <hamohammed.sa@gmail.com>
2017-03-23 01:27:14 +0300

    15 sigma

--- shaders/nodes_2DHeatMap_tex_frag.glsl
+++ shaders/nodes_2DHeatMap_tex_frag.glsl
@@ -12,14 +12,14 @@ in vec2             G_fragTexCoord;
 
 const float gaussian_steps[7] = float[7](0.0, 1.0, -1.0, 2.0, -2.0, 3.0, -3.0);
 
-const float gaussian_kernel13[49] = float[49](
-            0.020894, 0.020833, 0.020833,  0.020649, 0.020649, 0.020346, 0.020346,
-            0.020833, 0.020771, 0.020771,  0.020588, 0.020588, 0.020286, 0.020286,
-            0.020833, 0.020771, 0.020771,  0.020588, 0.020588, 0.020286, 0.020286,
-            0.020649, 0.020588, 0.020588,  0.020406, 0.020406, 0.020107, 0.020107,
-            0.020649, 0.020588, 0.020588,  0.020406, 0.020406, 0.020107, 0.020107,
-            0.020346, 0.020286, 0.020286,  0.020107, 0.020107, 0.019811, 0.019811,
-            0.020346, 0.020286, 0.020286,  0.020107, 0.020107, 0.019811, 0.019811
+const float gaussian_kernel15[49] = float[49](
+            0.020773, 0.020727, 0.020727,  0.020589, 0.020589, 0.020362, 0.020362,
+            0.020727, 0.020681, 0.020681,  0.020543, 0.020543, 0.020316, 0.020316,
+            0.020727, 0.020681, 0.020681,  0.020543, 0.020543, 0.020316, 0.020316,
+            0.020589	, 0.020543, 0.020543,  0.020407, 0.020407, 0.020182, 0.020182,
+            0.020589	, 0.020543, 0.020543,  0.020407, 0.020407, 0.020182, 0.020182,
+            0.020362, 0.020316, 0.020316,  0.020182, 0.020182, 0.019959, 0.019959,
+            0.020362, 0.020316, 0.020316,  0.020182, 0.020182, 0.019959, 0.019959
  );
 
 float getSplattedTexture(in sampler2D texture_toSplat, in vec2 coord);
@@ -28,7 +28,6 @@ void main() {
     float splat = getSplattedTexture(tex, G_fragTexCoord);
     vec4 red_color = vec4(1.0, 0.0, 0.0, 1.0);
     vec4 t_color = texture(tf, splat);
-
     vec4 mix_color = mix(t_color, red_color, splat);
     outcol = mix_color;
 }
@@ -49,7 +48,7 @@ float getSplattedTexture(in sampler2D texture_toSplat, in vec2 coord)
         for (int i = 0; i < 7; i++)
         {
             idx_offset.x = gaussian_steps[i] * stepXY.x; //step_x
-            weight = gaussian_kernel13[i + j * 7];
+            weight = gaussian_kernel15[i + j * 7];
             vec2 new_coord = coord.xy + idx_offset.xy;
             vec4 tex_value4 = texture(texture_toSplat, new_coord);
             result = result + (weight * tex_value4.r);
