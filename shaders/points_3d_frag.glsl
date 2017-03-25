34ce99b441209aa18e39a6ebd92266f5601e86fd
Haneen Mohammed <hamohammed.sa@gmail.com>
2017-03-25 21:14:46 +0300

    diamond shape for nodes with mito

--- shaders/points_3d_frag.glsl
+++ shaders/points_3d_frag.glsl
@@ -12,34 +12,35 @@ vec4  mat_specular      = vec4(1);
 vec4  light_specular    = vec4(1);
 vec3  lightDir          = vec3(0.5, 0.0, -0.9);
 
+float triangleArea(vec2 p1, vec2 p2, vec2 p3)
+{
+    float area = 0.5 * (p1.x * (p2.y - p3.y) + p2.x * ( p3.y - p1.y) + p3.x * (p1.y - p2.y));
+    return abs(area);
+}
+
 void main() {
 
-    if ( hasMito == 1 && node2D_alpha == 0)  {
+    if (   hasMito == 1 && node2D_alpha == 0  )  {
         vec4 node2d = vec4(color_val.rgb, alpha);
-        float maxX = 0.9;
-        float minX = 0.1;
-        float maxY = 0.9;
-        float minY = 0.1;
-        if (!(gl_PointCoord.x < maxX && gl_PointCoord.x >= minX &&
-              gl_PointCoord.y <= maxY && gl_PointCoord.y > minY)) {
-            node2d  -= vec4( 0.3, 0.3, 0.3, 1.0 );
+
+        vec2 p0 = vec2(0.5, 1.0);
+        vec2 p1 = vec2(1.0, 0.5);
+        vec2 p2 = vec2(0.5, 0.0);
+        vec2 p3 = vec2(0.0, 0.5);
+
+        float halfRect1 = triangleArea(p0, p1, p2);
+        float halfRect2 = triangleArea(p0, p2, p3);
+        float sumArea = halfRect1 + halfRect2;
+        // 1) get the sum
+        float p0Xp1 = triangleArea(p0, gl_PointCoord, p1);
+        float p1Xp2 = triangleArea(p1, gl_PointCoord, p2);
+        float p2Xp3 = triangleArea(p2, gl_PointCoord, p3);
+        float p3Xp0 = triangleArea(p3, gl_PointCoord, p0);
+        float allsum = p0Xp1 + p1Xp2 + p2Xp3 + p3Xp0;
+
+        if (  allsum >  sumArea ) {
+             discard;
         }
-
-//        vec2 p0 = vec2(0.5, 1.0);
-//        vec2 p1 = vec2(1.0, 0.5);
-//        vec2 p2 = vec2(0.5, 0.0);
-//        vec2 p3 = vec2(0.0, 0.5);
-
-//        float value = (p1.x - p0.x)*(gl_PointCoord.y -  p0.y) - (gl_PointCoord.x - p0.x)*(p1.y - p0.y);
-//        if (value > 0)
-//            discard;
-
-//        p0 = p1;
-//        p1 = p2;
-//        value = (p1.x - p0.x)*(gl_PointCoord.y -  p0.y) - (gl_PointCoord.x - p0.x)*(p1.y - p0.y);
-//        if (value > 0)
-//            discard;
-
 
         outcol = node2d;
     } else {
